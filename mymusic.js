dfb177943efe6dfc1b85be74b449e31fe6db6a9f
Midnight000 <873501007@qq.com>
2020-02-12 00:14:02 +0800

    aaa

--- mymusic.js
+++ mymusic.js
@@ -1,6 +1,6 @@
 var musicList=document.getElementsByTagName("li");
 var musicNode=document.getElementsByTagName("audio")[0];
-var musicSrclist=['http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_55923158&response=res&type=convert_url&'];
+var musicSrclist=['打上花火.flac'];
 var len=musicList.length;
 musicNode.src=musicSrclist[0];
 
