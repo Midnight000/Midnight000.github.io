d10e33fb80aa4088acdad8de6b9eb703895cf09b
Midnight000 <873501007@qq.com>
2020-02-12 10:06:26 +0800

    update

--- mymusic.js
+++ mymusic.js
@@ -1,14 +1,36 @@
-var musicList=document.getElementsByTagName("li");
-var musicNode=document.getElementsByTagName("audio")[0];
-var musicSrclist=['打上花火.flac'];
-var len=musicList.length;
-musicNode.src=musicSrclist[0];
+var musicList = document.getElementsByTagName("li");
+var musicNode = document.getElementsByTagName("audio")[0];
+var musicSrcList = ["http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_55923158&response=res&type=convert_url&",
+"http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_28510075&response=res&type=convert_url&",
+"http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_40602735&response=res&type=convert_url&"];
+var len = musicList.length;
+musicNode.src = musicSrcList[0];
+musicList[0].className="playing";
+for (var i = 0; i < len; i++) {
+  (function (i) {
+    musicList[i].onclick = function () {
+      if(musicList[i].className==="playing")return;
+      musicNode.src = musicSrcList[i];
+      musicNode.load();
+      musicNode.play();
+      for(var j=0;j<len;j++)musicList[j].className="";
+      this.className="playing";
+    }
+  })(i);
+}
 
-for(var i=0;i<len;i++){
-  musicList[i].onclick=function () {
-    musicNode.src="http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_55923158&response=res&type=convert_url&";
-    musicNode.load();
-    musicNode.play();
-  }
+musicNode.onended=function () {
+  var index=getPlaying()+1;
+  if(index===len)index=0;
+  musicNode.src = musicSrcList[index];
+  musicNode.load();
+  musicNode.play();
+  for(var j=0;j<len;j++)musicList[j].className="";
+  musicList[index].className="playing";
 }
 //需实现点相同歌无反应
+function getPlaying(){
+  for(var i=0;i<len;i++){
+    if(musicList[i].className==="playing")return i;
+  }
+}
